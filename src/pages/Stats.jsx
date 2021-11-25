595102366809741e96a1c2b3e6ceae8b1de80e73
D3vle0 <44903787+D3vle0@users.noreply.github.com>
2021-11-25 15:00:46 +0900

    fix: jsx-a11y/img-has-alt

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -4,11 +4,11 @@ export default function Stats() {
             <div className="stats">
                 <h1>Stats</h1>
                 <a href="https://github.com/d3vle0" target="_blank" rel="noreferrer">
-                    <img align="center" src="https://github-readme-stats.vercel.app/api?username=d3vle0&show_icons=true&theme=tokyonight&count_private=true" />
+                    <img align="center" src="https://github-readme-stats.vercel.app/api?username=d3vle0&show_icons=true&theme=tokyonight&count_private=true" alt="my github stats - overview" />
                 </a>
                 <br></br><br></br>
                 <a href="https://github.com/d3vle0" target="_blank" rel="noreferrer">
-                    <img align="center" src="https://github-readme-stats.vercel.app/api/top-langs/?username=D3vle0&layout=compact&hide=css,html" />
+                    <img align="center" src="https://github-readme-stats.vercel.app/api/top-langs/?username=D3vle0&layout=compact&hide=css,html" alt="my github stats - top langs" />
                 </a>
             </div>
         </>
